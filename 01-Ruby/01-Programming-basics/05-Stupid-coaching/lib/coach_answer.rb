f801de85eacec0bbcbc9773b3c9bfed0aacaccdf
oliviervo <olivier_vo@outlook.com>
2014-07-08 23:44:08 +0200

    Exercice calcu age V2

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,8 +1,20 @@
-def coach_answer(your_message)
-  # TODO: return coach answer to your_message
+def coach_answer(mon_message)
+  if mon_message.include? "?"
+    puts "Silly question, get dressed and go to work "
+
+  else
+    puts"I don't care son, get dressed and go to work !"
+
+
+  end
 end
 
-def coach_answer_enhanced(your_message)
+def coach_answer_enhanced(mon_message)
   # TODO: return coach answer to your_message, with additional custom rules of yours !
+  if mon_message.include? "jamais"
+    puts "#{coach_answer}, dégonglé"
+
+  end
 end
+
 
--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,3 +1,23 @@
 require_relative "coach_answer"
 #TODO: Implement the program that makes you discuss with your coach from the terminal.
 
+def question
+
+  puts "Posez une question au Coach : "
+mon_message=gets.chomp
+coach_answer(mon_message)
+
+coach_answer_enhanced(mon_message)
+
+
+until mon_message="I am going to work right now SIR !"
+
+  puts "Posez une autre question au Coach : "
+mon_message=gets.chomp
+coach_answer(mon_message)
+
+coach_answer_enhanced(mon_message)
+ end
+
+end
+
