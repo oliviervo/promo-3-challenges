8a93b9f9babe0a91a39ab060932464804097d6a3
oliviervo <olivier_vo@outlook.com>
2014-07-10 12:55:32 +0200

    loop with coach and interface

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,9 +1,12 @@
 def coach_answer(mon_message)
-  if mon_message.include? "?"
-    puts "Silly question, get dressed and go to work "
+  if mon_message.include?("?")
+    return "Silly question, get dressed and go to work !"
 
-  else
-    puts"I don't care son, get dressed and go to work !"
+  elsif mon_message=="I am going to work right now SIR !"
+    return ""
+
+    else mon_message !=mon_message.include?("?")
+      return "I don't care son, get dressed and go to work !"
 
 
   end
@@ -11,8 +14,8 @@ end
 
 def coach_answer_enhanced(mon_message)
   # TODO: return coach answer to your_message, with additional custom rules of yours !
-  if mon_message.include? "jamais"
-    puts "#{coach_answer}, dégonglé"
+  if mon_message.include? ("jamais")
+    puts "#{coach_answer}, allez!"
 
   end
 end
--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -5,9 +5,9 @@ def question
 
   puts "Posez une question au Coach : "
 mon_message=gets.chomp
-coach_answer(mon_message)
+return coach_answer(mon_message)
 
-coach_answer_enhanced(mon_message)
+return coach_answer_enhanced(mon_message)
 
 
 until mon_message="I am going to work right now SIR !"
