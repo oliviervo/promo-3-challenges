200bed4770b7bfe3b4aeb9b86e3553fbaa14492b
oliviervo <olivier_vo@outlook.com>
2014-07-10 11:22:31 +0200

    iterateur V1

--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
@@ -2,16 +2,47 @@ require_relative 'black_jack'
 
 def play_game
   #TODO: make the user play from terminal in a while loop that will stop when the user will not be asking for  a new card
+score=0
+bank=16
+
+return asking_for_card?(score)
+
+
 end
 
 def state_of_the_game(score, bank)
   # TODO: Returns custom message with player's score and bank's score
+puts "your score is #{score}, bank is #{bank}"
+
 end
 
 def asking_for_card?(score)
-  # TODO: Ask for a card only if the score is less or equal to 21, then returns true or false according to the user's choice
+if score<=21
+  puts "Card ? (type 'Y' or 'yes' for a new card)"
+  choix=gets.chomp
+
+end
+
+if choix=='Y' || choix=='yes'
+  score=score+ pick_card
+  state_of_the_game(score, bank)
+
+else
+  state_of_the_game(score, bank)
+
+
 end
 
 def build_message_for(outcome)
   #TODO: return specific message depending on the game outcome (= bank's score and user's score)
+while
+if outcome[bank]>outcome[score]
+  puts "Vous avez perdu"
+
+elsif outcome[bank]<outcome[score]
+  puts "Vous avez gagné"
+
+else
+  puts "match nul"
+
 end
--- 01-Ruby/03-Blocks/01-Enumerable/lib/enumerable.rb
+++ 01-Ruby/03-Blocks/01-Enumerable/lib/enumerable.rb
@@ -1,33 +1,56 @@
 def sum_odd_indexed(array)
-  # TODO: computes the sum of elements at odd indices (1, 3, 5, 7, etc.) 
+  # TODO: computes the sum of elements at odd indices (1, 3, 5, 7, etc.)
   #       You should make use Enumerable#each_with_index
+  sum=0
+
+  array.each_with_index do |x,index|
+    if index%2==1
+      sum=sum+x
+    end
+  end
+
+  return sum
 end
- 
+sum_odd_indexed([1,2,3])
+
+
 def even_numbers(array)
   # TODO: Return the even numbers from a list of integers.
   #       You should use Enumerable#select
+array.select {|number| number.even? }
+
 end
+even_numbers([1,2,3,8,9])
 
 def short_words(array, max_length)
   # TODO: Take and array of words, return the array of words not exceeding max_length characters
   #       You should use Enumerable#reject
+array.reject do|w|
+  if
+    w.length>max_length
+    end
+  end
+
 end
- 
+short_words(["ami","maison","ordinateur","chaise","instituteur"], 5)
+
+
+
 def first_under(array, limit)
   # TODO: Return the first number from an array that is less than limit.
   #       You should use Enumerable#find
 end
- 
+
 def add_bang(array)
   # TODO: Take an array of strings and return a new array with "!" appended to each string.
   #       You should use Enumerable#map
 end
- 
+
 def product(array)
   # TODO: Calculate the product of an array of numbers.
   #       You should use of Enumerable#reduce
 end
- 
+
 def sorted_pairs(array)
   # TODO: Reorganize an array into slices of 2 elements, and sort each slice alphabetically.
   #       You should make use of Enumerable#each_slice
