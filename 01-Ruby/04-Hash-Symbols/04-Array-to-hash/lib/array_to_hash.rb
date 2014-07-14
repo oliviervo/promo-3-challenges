b58ac609098042d053b2698463f8b97a4e6d3ac4
oliviervo <olivier_vo@outlook.com>
2014-07-14 16:15:07 +0200

    Tableau en hash

--- 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
+++ 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
@@ -1,3 +1,13 @@
+
+
 def array_to_hash(array)
   #TODO: implement the method :)
+
+  hash = Hash.new
+  array.each_with_index {|x, value|
+  hash[value] = x
+    }
+
+  hash
 end
+array_to_hash(["a", "b", "c"])
