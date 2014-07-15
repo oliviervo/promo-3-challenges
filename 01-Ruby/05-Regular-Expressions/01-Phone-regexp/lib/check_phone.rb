6d6026994c15b02ae2cf5b7ba67124630911eab1
oliviervo <olivier_vo@outlook.com>
2014-07-15 16:20:03 +0200

    Handling CSV

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,3 +1,5 @@
 def french_phone_number?(phone_number)
   # TODO: true or false?
+  if phone_number =~ /(0)\d{9}/
+    puts "Format de numéro valide"
 end
--- 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
+++ 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
@@ -1,6 +1,17 @@
 # Encoding: utf-8
 require 'csv'
+csv_options = { col_sep: ',', quote_char: '"', headers: :first_row, encoding: "ISO8859-1" }
+
+ CSV.foreach("/code/....", csv_options) do |row|
+
+ end
+
+
+
 
 def most_successfull(number, max_year, file_name)
   #TODO: return the number most successfull movies max_year
+puts my_array[0]["Movie"]
+
+
 end
