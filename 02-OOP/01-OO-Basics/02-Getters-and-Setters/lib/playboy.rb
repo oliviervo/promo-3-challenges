5dec58be07555c11bad864352c5b1c318dc880dc
oliviervo <olivier_vo@outlook.com>
2014-07-16 12:29:37 +0200

    Getters and setters

--- 02-OOP/01-OO-Basics/02-Getters-and-Setters/lib/playboy.rb
+++ 02-OOP/01-OO-Basics/02-Getters-and-Setters/lib/playboy.rb
@@ -1,6 +1,8 @@
 class Playboy
 
   #TODO: implement necessary getters or setters to run meeting_casanova.rb
+  attr_reader :conquests, :nationality
+  attr_accessor :name, :hair_length
 
   INITIAL_HAIR_LENGTH = 20  # beautiful long playboy hair
 
