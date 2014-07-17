b6a779b2f378062ca229107882f6cc0b2cce93ca
oliviervo <olivier_vo@outlook.com>
2014-07-17 15:34:15 +0200

    Self and priority

--- 02-OOP/02-OO-Advanced/03-About-self/lib/animal.rb
+++ 02-OOP/02-OO-Advanced/03-About-self/lib/animal.rb
@@ -2,26 +2,30 @@ class Animal
   # Modify the following methods to enable chaining !
   def name(name)
     @name = name
+    self
     #TODO: find good instruction
   end
-  
+
   def specie(specie)
     @specie = specie
+    self
     #TODO: find good instruction
   end
-  
+
   def color(color)
     @color = color
+    self
     #TODO: find good instruction
   end
-  
+
   def natural_habitat(natural_habitat)
     @natural_habitat = natural_habitat
+    self
     #TODO: find good instruction
   end
-  
+
   def to_s
     "Name: #{@name}, Specie: #{@specie}, Color: #{@color}, Natural Habitat: #{@natural_habitat}"
   end
-  
+
 end
