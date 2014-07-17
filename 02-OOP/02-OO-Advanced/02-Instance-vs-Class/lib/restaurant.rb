dcf65019264833c085fd13d3b2bc4d7079729542
oliviervo <olivier_vo@outlook.com>
2014-07-17 12:10:15 +0200

    Average rate accoridng to new  user input

--- 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
@@ -1,11 +1,24 @@
 class Restaurant
-  
+attr_accessor :average_rating, :city, :name, :address
   #TODO add relevant accessors if necessary
-  
+
   def initialize(city, name)
     #TODO: implement constructor with relevant instance variables
+    @city = city
+    @name = name
+    @average_array = []
+    @average_rating = 0
+    @address = address
   end
-  
+
   #TODO: implement #filter_by_city and #rate methods
-  
+  def rate(user_rate)
+    @average_array << user_rate.to_f
+    @average_rating = @average_array.inject(:+) / @average_array.size
+  end
+
+  def filter_by_city(restaurants, city)
+
+  end
+
 end
