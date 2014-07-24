b672b953b3602d2774739738a482401ed0db2fd5
oliviervo <olivier_vo@outlook.com>
2014-07-24 15:52:29 +0200

    Jukebox SQL with ruby

--- 03-AR-Database/01-DB-Design-and-SQL/03-Interacting-with-code/lib/queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/03-Interacting-with-code/lib/queries.rb
@@ -6,16 +6,28 @@ db = SQLite3::Database.new(database_path)
 
 def number_of_rows(db, table_name)
   #TODO: count number of rows in table table_name
+db.execute( "SELECT COUNT(*)FROM #{table_name}" )
 end
 
 def sorted_artists(db)
   #TODO: return array of artists' names sorted alphabetically
+ db.execute("SELECT Name FROM Artist ORDER BY Name ASC")
+
 end
 
 def love_tracks(db)
   #TODO: return array of love songs
+  db.execute("SELECT Name FROM Track WHERE Name LIKE '%love%' ORDER BY Name ASC;")
 end
 
 def long_tracks(db, min_length)
   #TODO: return tracks verifying: duration > min_length (minutes)
+  min_length_in_milliseconds = min_length * 60000
+  db.execute("SELECT Name FROM Track WHERE Milliseconds > #{min_length_in_milliseconds} ORDER BY Milliseconds DESC;")
+
 end
+
+puts number_of_rows(db, "Album")
+puts sorted_artists(db)
+p love_tracks(db)
+puts long_tracks(db, 6)
