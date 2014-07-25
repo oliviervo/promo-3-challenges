21e26742dc83329bac5f08b01e0bdf82e6c9165f
oliviervo <olivier_vo@outlook.com>
2014-07-25 12:19:38 +0200

    DAtabase and POsts

--- 03-AR-Database/02-SQL-CRUD/lib/crud.rb
+++ 03-AR-Database/02-SQL-CRUD/lib/crud.rb
@@ -1,32 +1,55 @@
-require 'sqlite3'
+require "sqlite3"
+
 
 def create_scheme(db)
   #TODO: create your db scheme
-  sql = %q{ 
-  }  # %q allows for multiline string in ruby 
+  sql = %q{CREATE TABLE IF NOT EXISTS posts
+    (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name VARCHAR(100),
+      rating INTEGER(2),
+      source VARCHAR (200),
+      date DATETIME (20)
+      )
+
+  }  # %q allows for multiline string in ruby
   db.execute(sql)
 end
 
 def create_post(db, post)
   #TODO: add a new post to your db
+  db.execute("INSERT INTO posts(name,source,date,rating)
+  VALUES ('#{post[:name]}','#{post[:source_url]}', '#{post[:date]}', #{post[:rating]})")
+  #(post[name:],post[rating:],post[source_url:],post[date:]);
+
 end
 
 def get_posts(db)
    #TODO: list all posts
+   all_posts = db.execute("SELECT * FROM posts")
+   p all_posts
+
 end
 
 def get_post(db, id)
   #TODO: get a specific post
+  getpost = db.execute("SELECT * FROM posts WHERE id = #{id}")
+  p getpost
 end
 
 def update_post(db, id, name)
 	#TODO: update a post's name in your db
+  updated_post = db.execute("UPDATE posts SET Name = '#{name}' WHERE id = #{id}")
+  puts updated_post
 end
 
 def delete_posts(db)
   #TODO: delete a post in your db
+  deletes_posts = db.execute("DELETE FROM posts")
+
 end
 
 def delete_post(db, id)
   #TODO: delete a specific post in your db
+  db.execute("DELETE FROM posts WHERE id = #{id}")
 end
--- 03-AR-Database/02-SQL-CRUD/lib/interface.rb
+++ 03-AR-Database/02-SQL-CRUD/lib/interface.rb
@@ -8,7 +8,7 @@ end
 db_path = "db/news.sqlite"
 db = SQLite3::Database.new(db_path)
 
-create_scheme(db) 
+create_scheme(db)
 
 while true
 
@@ -16,11 +16,15 @@ while true
   puts "1. Create a post"
   puts "2. Read your posts"
   puts "3. Delete all posts"
-  puts "4. Exit"
-  
+  puts "4. List all posts"
+  puts "5. Read a specific post"
+  puts "6. Update a specific post"
+  puts "7. Delete all posts"
+  puts "8. Delete a specific posts"
+  puts "9. Exit"
   # TODO: add other CRUD tasks if you wish!
 	choice =  gets.chomp.to_i
-	
+
 	case choice
   when 1
     name = ask_and_get("name")
@@ -32,8 +36,22 @@ while true
     get_posts(db)
   when 3
     delete_posts(db)
-  when 4 
+  when 4
+    get_posts(db)
+  when 5
+    id = ask_and_get(id)
+    get_post(db, id)
+  when 6
+    id = ask_and_get(id)
+    name = ask_and_get(name)
+    update_post(db, id, name)
+  when 7
+    delete_posts(db)
+  when 8
+    id = ask_and_get(id)
+    delete_post(db, id)
+  when 9
     break
-	end 
-	
+	end
+
 end
