440eec5f7617dcc4fdc41ebbfe86772be363376d
oliviervo <olivier_vo@outlook.com>
2014-08-04 16:22:40 +0200

    JQuery 101

--- 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
+++ 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
@@ -8,45 +8,47 @@
 function run_challenges() {
 
   // Ex 1. Read the content of the email input
-  var email = null;  // TODO: replace null, keep the email variable.
+  var email = $('#email').val();  // TODO: replace null, keep the email variable.
 
 
   // Ex 2. Fill the content of the email input with your email
-
-
+var newmail = $('#email').val('qqqss.com')
 
   // Ex 3. Replace the email hint (next to the input) with 'This is my email now'
   //       The text should be emphasized using a <strong> tag
-
+var message = $('#email-hint').html("<strong>This is my email now</strong>");
 
   // Ex 4. Add the .blue CSS class to the table header cells (th elements)
-
+var tableblue = $('th').addClass('blue')
 
 
   // Ex 5. Count the number of table body rows there are (team count!)
-  var team_count = 0;  // TODO: replace 0, keep the team_count variable.
+  var team_count = $('tbody tr').length;  // TODO: replace 0, keep the team_count variable.
 
 
 
   // Ex 6. Say there is a 15th team added to the table.
   //       Add a row at the bottom, this new team should have zero points.
-
+var add_team = $('tbody').append('<tr><td>15</td></tr>');
 
 
   // Ex 7. Write some code to sum all points given to all teams
-  var sum = 0;  // TODO: replace 0 with your sum result, keep the sum variable.
+  var sum = $('tbody tr td td td');  // TODO: replace 0 with your sum result, keep the sum variable.
 
 
 
   // Ex 8. Change the background color of all table header cells to #DDF4FF
-
-
+  var ChangeBackgroundColor = $('th').css("background-color", "#DDF4FF");
 
   // Ex 9. Translate the #results list 20px downward (animation would be great ^_^)
+  var transalteDown = $('#results').css("margin-top", "-20px");
+
 
 
 
   // Ex 10. Remove the "Email:" label from the DOM
+
+  var RemoveEmailLabel = $('label').replaceWith();
 
 
 
